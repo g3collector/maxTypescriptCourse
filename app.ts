2a3a6ebbc6d1331870e718cd30e5570fa98501b6
Irdi Avxhi <irdiavxhi@gmail.com>
2018-06-18 01:54:57 +0200

    generics

--- app.ts
+++ app.ts
@@ -6,3 +6,19 @@ function echo(data : any){
 
 console.log(echo('irdi'))
 console.log(echo(22));
+
+//Better Generic
+function betterEcho<T>(data:T){
+ return data;
+}
+
+
+console.log(betterEcho('irdi').length)
+// console.log(betterEcho(22).length); //can use a number and then i can't use not a number property after
+
+
+//build in generics
+const testResults : Array<number> = [1.9];
+testResults.push(22);
+console.log(...testResults);
+
